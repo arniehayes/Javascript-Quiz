a14042525d07d914541c17f9b14c0a68eea62bc6
Arnie Hayes <arniehayes1@gmail.com>
2022-04-14 18:20:49 -0700

    safari font fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "881acca1894a981e"; }
+/******/ 		__webpack_require__.h = function() { return "9e3c88139052425d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

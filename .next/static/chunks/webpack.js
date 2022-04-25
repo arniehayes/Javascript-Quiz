6d7a3d3d814e1c8d949aa58152b138b44e181b41
Arnie Hayes <arniehayes1@gmail.com>
2022-04-24 20:41:33 -0700

    safari trick

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ce85d472479c9b8"; }
+/******/ 		__webpack_require__.h = function() { return "41ac9ad9470e8e0c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.scss'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" type="text/css" />
+      </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
 export default MyApp

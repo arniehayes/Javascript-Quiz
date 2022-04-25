b930c5653975bc0757fb36705627f995ee68fc89
Arnie Hayes <arniehayes1@gmail.com>
2022-04-24 21:36:54 -0700

    font size fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c2d38bbf3a9866b4"; }
+/******/ 		__webpack_require__.h = function() { return "fd31c1a05d3b2385"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
